ee997cc16d8ff96c30ed821b6b7778c82278c162
Brian Bixby <brianbixby0@gmail.com>
2018-09-26 17:49:03 -0700

    pushing

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from '../navbar';
 import LandingContainer from '../landing-container';
 import ProfileContainer from '../profile-container';
-import Play from '../map-container';
+import MapContainer from '../map-container';
 import { signIn, tokenSignInRequest } from '../../actions/userAuth-actions.js';
 import { userProfileFetchRequest } from '../../actions/userProfile-actions.js';
 
@@ -17,7 +17,7 @@ class App extends React.Component {
           <Route path='*' component={Navbar} />
           <Route exact path='/' component={LandingContainer} />
           <Route exact path='/user/:profileID' component={ProfileContainer} />
-          <Route exact path='/play' component={Play} />
+          <Route exact path='/play' component={MapContainer} />
         </section>
       </BrowserRouter>
     );
--- src/components/map-container/index.js
+++ src/components/map-container/index.js
@@ -1,78 +1,55 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import { connect } from 'react-redux';
-
-import { signUpRequest, signInRequest } from '../../actions/userAuth-actions.js';
-import { userProfileFetchRequest } from '../../actions/userProfile-actions.js';
-
-export class MapContainer extends React.Component {
+import PropTypes from 'prop-types';
+ 
+class MapContainer extends React.Component {
     constructor(props) {
-    super(props);
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
+        super(props);
+    
+        this.state = {
+          currentLocation: {
+            lat:  7,
+            lng:  -12
+          }
+        }
     }
-    // binding this to event-handler functions
-    // this.onMarkerClick = this.onMarkerClick.bind(this);
-    // this.onMapClick = this.onMapClick.bind(this);
-  }
-    onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true,
-    });
-  };
-
-  onMapClick = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
+    componentDidMount() {
+        this.updatePosition();
     }
-  };
+    updatePosition(){
+        if (navigator && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                this.setState({
+                    currentLocation: {
+                        lat: pos.coords.latitude,
+                        lng: pos.coords.longitude
+                    }
+                })
+                console.log('state: ', this.state.currentLocation);
+            })
+        }
+        else
+            console.log('no');
+    }
+    render() {
+        const style = {
+            width: '100%',
+            height: '100%'
+          }
 
-  render() {
-    const style = {
-      width: '100%',
-      height: '100%'
-    };
-    return (
-      <Map google={this.props.google} onClick={this.onMapClick}
-          style={style}
-          initialCenter={{
-            lat: 40.854885,
-            lng: -88.081807
-          }}
-          zoom={15}
-          onClick={this.onMapClick}
-        >
-        <Marker onClick={this.onMarkerClick} name={'Current location'} />      
-        <InfoWindow onClose={this.onInfoWindowClose} marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+
+        return (
+            <Map google={this.props.google} zoom={14} center={this.state.currentLocation}>
+                <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <p>hi</p>
+                </InfoWindow>
+            </Map>
+        )
+    }
 }
 
-let mapStateToProps = state => ({
-  userAuth: state.userAuth,
-  userProfile: state.userProfile,
-});
-
-let mapDispatchToProps = dispatch => {
-  return {
-    signUp: user => dispatch(signUpRequest(user)),
-    signIn: user => dispatch(signInRequest(user)),
-    userProfileFetch: () => dispatch(userProfileFetchRequest()),
-  };
-};
-
-export default (connect(mapStateToProps, mapDispatchToProps), GoogleApiWrapper({
-  apiKey: (process.env.__GAPI_KEY__)
-})(MapContainer));
+export default GoogleApiWrapper({
+    apiKey: process.env.__GAPI_KEY__
+  })(MapContainer)
